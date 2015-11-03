60dbd7eaba1a98771d06f547bca444404cf73109
hunotika <csaszarhunor@gmail.com>
2015-11-03 14:43:50 +0100

    add validate_email and the corresponding test

--- validation.py
+++ validation.py
@@ -5,3 +5,9 @@ def validate_name(name_string):
 	name = name_string.split()
 	return name_string.isalpha() and len(name) > 1
 
+
+def validate_email(email_string: str):
+	return '@' in email_string and email_string.endswith(('.hu', '.com')) and \
+		email_string.index('@') > 0
+
+
--- validation_tests.py
+++ validation_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from blood.validation import validate_name
+from blood.validation import validate_name, validate_email
 
 
 class ValidateName(unittest.TestCase):
@@ -8,6 +8,20 @@ class ValidateName(unittest.TestCase):
 
 	def test_number_of_names(self):
 		self.assertFalse(validate_name('Joska'))
+
+
+class ValidEmail(unittest.TestCase):
+	def test_email_contains_at(self):
+		self.assertFalse(validate_email('jdat.com'))
+
+	def test_email_ending_wrong(self):
+		self.assertFalse(validate_email('j@dat.con'), validate_email('j@dat.net'))
+
+	def test_email_ending_right(self):
+		self.assertTrue(validate_email('j@dat.com'), validate_email('j@dat.hu'))
+
+	def test_email_not_starts_with_at(self):
+		self.assertFalse(validate_email('@dat.com'))
 
 
 if __name__ == '__main__':
