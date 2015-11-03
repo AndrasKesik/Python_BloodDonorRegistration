247477a3a28584b3396f3d5fb0fffac9d1dfcf2f
zavox <amdras.kesik@gmail.com>
2015-11-03 14:05:48 +0100

    struktura

--- validation.py
+++ validation.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-def valid_gender(gender_string):
-    return gender_string.upper() == "F" or gender_string.upper() == "M"
-=======
+# -*- coding: UTF-8 -*-
 
 
-def validate_name(name_string):
-    name = name_string.split()
-    return name_string.isalpha() and len(name) > 1
-
->>>>>>> aabdb4a9d9bb868ff5d3652e00d18ead4167b8dd
+class ValidateName():
+    @staticmethod
+    def valid_gender(gender_string):
+        return gender_string.upper() == "F" or gender_string.upper() == "M"
+    @staticmethod
+    def validate_name(name_string):
+        name = name_string.split()
+        return name_string.isalpha() and len(name) > 1
--- validation_tests.py
+++ validation_tests.py
@@ -1,25 +1,20 @@
+# -*- coding: UTF-8 -*-
+
 import unittest
-<<<<<<< HEAD
-from blood.validation import valid_gender
-
-class validate_gender(unittest.TestCase):
-    def test_valid_input(self):
-        self.assertTrue(valid_gender("f"))
-
-if __name__ == '__main__':
-    unittest.main()
-=======
-from blood.validation import validate_name
+from blood.validation import ValidateName
 
 
-class ValidateName(unittest.TestCase):
+class ValidateNameTests(unittest.TestCase):
     def test_name_contains_only_letter(self):
-        self.assertFalse(validate_name('Joska1'))
+        self.assertFalse(validation.validate_name('Joska1'))
 
     def test_number_of_names(self):
         self.assertFalse(validate_name('Joska'))
 
+     def test_gender(self):
+        self.assertTrue(valid_gender("f"))
+
+
 
 if __name__ == '__main__':
     unittest.validation()
->>>>>>> aabdb4a9d9bb868ff5d3652e00d18ead4167b8dd
