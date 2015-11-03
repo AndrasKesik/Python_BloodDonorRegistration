1e6b5a8e10ec9a3199fca791a277b3531cf38d44
Kocsis Viktor <achilleus88888@gmail.com>
2015-11-03 13:46:31 +0100

    gender cucc

--- validation.py
+++ validation.py
@@ -1,1 +1,2 @@
-__author__ = 'kesikandras'
+def valid_gender(gender_string):
+    return gender_string.upper() == "F" or gender_string.upper() == "M"
--- validation_tests.py
+++ validation_tests.py
@@ -1,2 +1,9 @@
-__author__ = 'kesikandras'
+import unittest
+from blood.validation import valid_gender
 
+class validate_gender(unittest.TestCase):
+    def test_valid_input(self):
+        self.assertTrue(valid_gender("f"))
+
+if __name__ == '__main__':
+    unittest.main()
