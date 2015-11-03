aabdb4a9d9bb868ff5d3652e00d18ead4167b8dd
hunotika <csaszarhunor@gmail.com>
2015-11-03 12:48:25 +0100

    Add validate_name and the corresponding test

--- validation.py
+++ validation.py
@@ -1,1 +1,7 @@
 __author__ = 'kesikandras'
+
+
+def validate_name(name_string):
+	name = name_string.split()
+	return name_string.isalpha() and len(name) > 1
+
--- validation_tests.py
+++ validation_tests.py
@@ -1,2 +1,14 @@
-__author__ = 'kesikandras'
+import unittest
+from blood.validation import validate_name
 
+
+class ValidateName(unittest.TestCase):
+	def test_name_contains_only_letter(self):
+		self.assertFalse(validate_name('Joska1'))
+
+	def test_number_of_names(self):
+		self.assertFalse(validate_name('Joska'))
+
+
+if __name__ == '__main__':
+	unittest.validation()
