d09e79a1994df55620d410584ff77991a83f048f
biroorsolya <birorso@gmail.com>
2015-11-04 13:00:12 +0100

    bloodtype, cities, integers, date validate

--- validation.py
+++ validation.py
@@ -1,4 +1,7 @@
 # -*- coding: UTF-8 -*-
+import datetime
+blood_type = ("A+", "A-", "B+", "B-", "AB+", "AB-", "0+", "0-")
+cities = ("Miskolc", "Kazincbacika", "Szerencs", "Sarospatak")
 
 
 class Validate():
@@ -11,7 +14,7 @@ class Validate():
     @staticmethod
     def validate_name(name_string):
         """Checks the name input"""
-        return name_string.isalpha() and len(name) > 1
+        return name_string.isalpha() and len(name_string) > 1
 
     @staticmethod
     def validate_email(email_string: str):
@@ -20,25 +23,38 @@ class Validate():
             email_string.index('@') > 0
 
     @staticmethod
-    def validate_date():
-        pass
+    def validate_date(date):
+        """Checks that the date format is correct"""
+        date_format = "%Y.%m.%d"
+        try:
+            datetime.datetime.strptime(date, date_format)
+        except:
+            return False
+        return True
+
+
 
     @staticmethod
     def validate_time():
         pass
 
     @staticmethod
-    def validate_positive_int():
-        pass
+    def validate_positive_int(number):
+        """Checks the weight, hemoglobin, bed and donor number is positive integer"""
+        return number.isdigit and int(number) > 1
 
     @staticmethod
-    def validate_blood_type():
-        pass
+    def validate_blood_type(type_of_blood):
+        """Checks that the blood type is in the given list"""
+        if type_of_blood.upper() in blood_type:
+            return True
 
 
     @staticmethod
-    def validate_city_name():
-        pass
+    def validate_city_name(city):
+        """Checks that the input is in the given list"""
+        if city.capitalize() in cities:
+            return True
 
     @staticmethod
     def validate_address():
