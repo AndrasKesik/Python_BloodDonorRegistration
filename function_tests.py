5c3250d2d7670e6901fde4231ef95ba30d6e03cf
hunotika <csaszarhunor@gmail.com>
2015-11-05 15:45:01 +0100

    function_test frase added

--- function_tests.py
+++ function_tests.py
@@ -3,14 +3,66 @@
 import unittest
 from functions import Donor
 
-class DonorTests(unittest.TestCase):
+
+# Donor Tests
+class ParseNameTests(unittest.TestCase):
     def test_parse_name(self):
         bela = Donor()
         self.assertEqual(1, len(bela.parse_name("bela")))
         self.assertEqual(2, len(bela.parse_name("movacs bela")))
         self.assertEqual(3, len(bela.parse_name("movacs bela pista")))
 
-class EventTests(unittest.TestCase):
+
+class SuitabilityTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class DonorAgeTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class IDExpirationTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class DocTypeTests(unittest.TestCase):
+    def test_valid_passport(self):
+        self.assertEqual(Donor.type_of_doc('ASDFGH12'), 'PASSPORT')
+
+    def test_valid_ID(self):
+        self.assertEqual(Donor.type_of_doc('123456AB'), 'ID')
+
+
+class DataOutTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class HemoglobinTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+# Event Tests
+class TenDaysRegTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class IsWeekdayTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class CalculateDurationTests(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse()
+
+
+class MaxDonorNumberTests(unittest.TestCase):
     def test_something(self):
         self.assertFalse()
 
