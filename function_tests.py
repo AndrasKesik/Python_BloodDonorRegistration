9e19f958642f8244e71b217625960fae7fe0dfa3
hunotika <csaszarhunor@gmail.com>
2015-11-05 20:04:09 +0100

    function tests, donor tests added

--- function_tests.py
+++ function_tests.py
@@ -26,18 +26,50 @@ class ParseNameTests(unittest.TestCase):
 
 
 class SuitabilityTests(unittest.TestCase):
-    def test_something(self):
-        self.assertFalse()
+    def test_too_skinny(self):
+        weight = 49
+        last_donation_date = datetime.strptime('2013.11.05', '%Y.%m.%d')
+        date_of_birth = datetime.strptime('1990.02.26', '%Y.%m.%d')
+        self.assertFalse(Donor.is_suitable(weight, last_donation_date, date_of_birth))
+
+    def test_too_early_last_donation(self):
+        weight = 75
+        date_of_testing = datetime.today()
+        last_donation_date = datetime.strptime('2013.11.05', '%Y.%m.%d')
+        date_of_birth = datetime.strptime('1990.02.26', '%Y.%m.%d')
+        self.assertFalse(Donor.is_suitable(weight, last_donation_date, date_of_birth))
+
+    def test_donor_too_young(self):
+        weight = 75
+        date_of_testing = datetime.today()
+        last_donation_date = date_of_testing - timedelta(days=31)
+        date_of_birth = date_of_testing - timedelta(days=750)
+        self.assertFalse(Donor.is_suitable(weight, last_donation_date, date_of_birth))
 
 
 class DonorAgeTests(unittest.TestCase):
-    def test_something(self):
-        self.assertFalse()
+    def test_donor_age(self):
+        date_of_testing = datetime.today()
+        birthday = datetime.strptime('1990.02.26', '%Y.%m.%d')
+        age = (birthday - date_of_testing).days // 365
+        self.assertEqual(Donor.donor_age(birthday), age)
 
 
 class IDExpirationTests(unittest.TestCase):
-    def test_something(self):
-        self.assertFalse()
+    def test_ID_expired_8_days_ago(self):
+        date_of_testing = datetime.today()
+        expired_date = date_of_testing - timedelta(days=8)
+        self.assertFalse(Donor.id_not_expired(expired_date))
+
+    def test_ID_expired_1_days_ago(self):
+        date_of_testing = datetime.today()
+        expired_date = date_of_testing - timedelta(days=1)
+        self.assertFalse(Donor.id_not_expired(expired_date))
+
+    def test_ID_not_expired(self):
+        date_of_testing = datetime.today()
+        expiration_date = date_of_testing + timedelta(days=3)
+        self.assertFalse(Donor.id_not_expired(expiration_date))
 
 
 class DocTypeTests(unittest.TestCase):
@@ -54,8 +86,7 @@ class DataOutTests(unittest.TestCase):
 
 
 class HemoglobinTests(unittest.TestCase):
-    def test_hemoglobin_too_high(self):
-        self.assertFalse(Donor.generate_hemoglobin_level())
+    pass
 
 
 # Event Tests
